fce1b76a0b1dca2461fa543b446a9003b7662f7f
surevil <445073309@qq.com>
2018-04-20 00:25:51 +0800

    try hard to complete alexnet but failed

--- njuTJ.MachineLearning/train.py
+++ njuTJ.MachineLearning/train.py
@@ -17,8 +17,11 @@ import numpy as np
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
 
+iterator = 6000
+
 # 数据获取
 classification = ["cbzw", "mbzw", "tbzw", "qgzw", "sgzw", "lkzw", "sszw", "drzw"]
+page_num = []
 user_agents = ['Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
                'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
                'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11']
@@ -29,9 +32,45 @@ url_page_index = []
 url_now_index = []
 
 for i in range(classification_len):
+    page_num.append(0)
     url_has_more.append(True)
     url_page_index.append(1)
     url_now_index.append(0)
+
+
+def get_next_batch():
+    index = random.randint(0, classification_len - 1)
+    agents_index = random.randint(0, user_agents.__len__() - 1)
+    headers = {
+        'User-Agent': user_agents[agents_index]
+    }
+    searchPage = url_page_index[index]
+    if not url_has_more[index]:
+        searchPage = random.randint(1, page_num[index])
+    url = "https://tuku.huabaike.com/default.php/Pics/ajaxupload/" + str(math.floor(time.time())) + "?page=" + str(
+        searchPage) + "&url=" + str(classification[index])
+    res = requests.get(url=url, headers=headers)
+    content = json.loads(res.content.decode('utf-8'))
+    length = content['length']
+    if length == 0:
+        page_num[index] = url_page_index[index]
+    soupAll = BeautifulSoup(content['htmlstr'], "html.parser")
+    imageUrl = soupAll.find_all('a')[url_now_index[index]]['href']
+    url_now_index[index] += 1
+    if length == url_now_index[index]:
+        url_page_index[index] += 1
+        url_now_index[index] = 0
+
+    image_bytes = urlopen(imageUrl).read()
+    data_stream = io.BytesIO(image_bytes)
+    with open("temp.jpg", "wb") as file:
+        file.write(data_stream.read())
+    input_x = cv2.imread("temp.jpg")
+    input_x = cv2.resize(input_x, (227, 227))
+    input_x = input_x.astype(np.float32)
+    input_x = np.reshape(input_x, [1, 227, 227, 3])
+    y = get_realY(index)
+    return [input_x, y]
 
 
 def resize(w, h, w_box, h_box, pil_image):
@@ -50,26 +89,27 @@ def get_realY(index):
             result.append(1)
         else:
             result.append(0)
+    return result
 
 
 class AlexNet(object):
     def __init__(self, input_x, keep_prob, num_classes, weights_path='alexnet.npy'):
-        # Initialization the parameters
         self.input_x = input_x
         self.keep_prob = keep_prob
         self.weights_path = weights_path
         self.num_classes = num_classes
-        # Create the AlexNet Network Define
-        self.create()
 
     def conv(self, x, kernel_height, num_kernels, stride, padding='SAME', padding_num=0, groups=1):
         input_channels = int(np.shape(x)[-1])
         if not padding_num == 0:
             x = tf.pad(x, [[0, 0], [padding_num, padding_num], [padding_num, padding_num], [0, 0]])
+        weights_initial = tf.truncated_normal(
+            shape=[kernel_height, kernel_height, input_channels, num_kernels], stddev=0.1)
+        weights = tf.Variable(weights_initial)
+        biases_initial = tf.constant(0.1, shape=[num_kernels])
+        biases = tf.Variable(biases_initial)
+
         convolve = lambda i, k: tf.nn.conv2d(i, k, strides=[1, stride, stride, 1], padding=padding)
-        weights = tf.get_variable('weights',
-                                  shape=[kernel_height, kernel_height, input_channels / groups, num_kernels])
-        biases = tf.get_variable('biases', shape=[num_kernels])
         if groups == 1:
             conv = convolve(x, weights)
         else:
@@ -79,8 +119,8 @@ class AlexNet(object):
 
             conv = tf.concat(axis=3, values=output_groups)
 
-        # add biases and avtive function
-        withBias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().as_list())
+        # add biases and active function
+        withBias = tf.add(conv, biases)
         relu = tf.nn.relu(withBias)
         return relu
 
@@ -92,8 +132,11 @@ class AlexNet(object):
         return tf.nn.local_response_normalization(input, depth_radius=radius, alpha=alpha, beta=beta, bias=bias)
 
     def fc(self, input, num_in, num_out, drop_ratio=0, relu=True):
-        weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
-        biases = tf.get_variable('biases', [num_out], trainable=True)
+        weights_initial = tf.truncated_normal(
+            shape=[num_in, num_out], stddev=0.1)
+        weights = tf.Variable(weights_initial)
+        biases_initial = tf.constant(0.1, shape=[num_out])
+        biases = tf.Variable(biases_initial)
         # Linear
         act = tf.nn.xw_plus_b(input, weights, biases)
 
@@ -109,15 +152,15 @@ class AlexNet(object):
             else:
                 return tf.nn.dropout(act, 1.0 - drop_ratio)
 
-    def create(self):
+    def pred(self):
         # layer 1
-        conv1 = self.conv(self.input_x, 11, 96, 4, padding='SAME')
-        pool1 = self.maxPooling(conv1, filter_size=3, stride=2, padding='SAME')
-        norm1 = self.lrn(pool1, 2, 2e-05, 0.75, name='norm1')
+        conv1 = self.conv(self.input_x, 11, 96, 4, padding='VALID')
+        pool1 = self.maxPooling(conv1, filter_size=3, stride=2, padding='VALID')
+        norm1 = self.lrn(pool1, 2, 2e-05, 0.75)
         # layer 2
         conv2 = self.conv(norm1, 5, 256, 1, padding_num=0, groups=2)
         pool2 = self.maxPooling(conv2, filter_size=3, stride=2, padding='VALID')
-        norm2 = self.lrn(pool2, 2, 2e-05, 0.75, name='norm2')
+        norm2 = self.lrn(pool2, 2, 2e-05, 0.75)
         # layer 3
         conv3 = self.conv(norm2, 3, 384, 1)
         # layer 4
@@ -132,51 +175,24 @@ class AlexNet(object):
         # layer 7
         fc7 = self.fc(input=fc6, num_in=4096, num_out=4096, drop_ratio=1.0 - self.keep_prob, relu=True)
         # layer 8
-        self.fc8 = self.fc(input=fc7, num_in=4096, num_out=self.num_classes, drop_ratio=0, relu=False)
+        fc8 = self.fc(input=fc7, num_in=4096, num_out=self.num_classes, drop_ratio=0, relu=False)
+        return fc8
 
 
-while True:
-    index = random.randint(0, classification_len - 1)
-    agents_index = random.randint(0, user_agents.__len__() - 1)
-    headers = {
-        'User-Agent': user_agents[agents_index]
-    }
-    if not url_has_more[index]:
-        isNotStopTime = False
-        for i in range(classification_len):
-            isNotStopTime = isNotStopTime | url_has_more[i]
-        if not isNotStopTime:
-            break
-        continue
-    url = "https://tuku.huabaike.com/default.php/Pics/ajaxupload/" + str(math.floor(time.time())) + "?page=" + str(
-        url_page_index[index]) + "&url=" + str(classification[index])
-    res = requests.get(url=url, headers=headers)
-    content = json.loads(res.content.decode('utf-8'))
-    length = content['length']
-    if length == 0:
-        url_has_more[index] = False
-        continue
-    soupAll = BeautifulSoup(content['htmlstr'], "html.parser")
-    imageUrl = soupAll.find_all('a')[url_now_index[index]]['href']
-    url_now_index[index] += 1
-    if length == url_now_index[index]:
-        url_page_index[index] += 1
-        url_now_index[index] = 0
+input_x = tf.placeholder(tf.float32, [None, 227 * 227 * 3])
+input_x = tf.reshape(input_x, [-1, 227, 227, 3])
+real_y = tf.placeholder(tf.float32, [None, 8])
+model = AlexNet(input_x, 0.5, 8)
+pred_y = model.pred()
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred_y, real_y))
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
-    w_box = 224
-    h_box = 224
-    image_bytes = urlopen(imageUrl).read()
-    data_stream = io.BytesIO(image_bytes)
-    x = cv2.imread(data_stream)
-    x = cv2.resize(x, (227, 227))
-    x = x.astype(np.float32)
-    x = np.reshape(x, [1, 227, 227, 3])
-    pred_y = tf.placeholder(tf.float32, [None, 8])
-    y = get_realY(index)
-    model = AlexNet(input, 0.5, 1000)
-    score = model.fc8
-    max = tf.arg_max(score, 1)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        model.load_weights(sess)
-        label_id = sess.run(max)[0]
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(iterator):
+        next_batch = get_next_batch()
+        sess.run(optimizer, feed_dict={input_x: next_batch[0], real_y: next_batch[1]})
+        if step % 1 == 0:
+            print(sess.run(accuracy, feed_dict={input_x: next_batch[0], real_y: next_batch[1]}))
