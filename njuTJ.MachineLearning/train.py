cff34602eaa4e5a8abf0522908dbf7d1d98f93b7
surevil <445073309@qq.com>
2018-04-21 16:11:55 +0800

    complete very simple alexnet

--- njuTJ.MachineLearning/train.py
+++ njuTJ.MachineLearning/train.py
@@ -18,6 +18,7 @@ if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 iterator = 6000
+num_classes = 8
 
 # 数据获取
 classification = ["cbzw", "mbzw", "tbzw", "qgzw", "sgzw", "lkzw", "sszw", "drzw"]
@@ -69,7 +70,7 @@ def get_next_batch():
     input_x = cv2.resize(input_x, (227, 227))
     input_x = input_x.astype(np.float32)
     input_x = np.reshape(input_x, [1, 227, 227, 3])
-    y = get_realY(index)
+    y = get_realY([index], 1)
     return [input_x, y]
 
 
@@ -82,117 +83,125 @@ def resize(w, h, w_box, h_box, pil_image):
     return pil_image.resize((width, height))
 
 
-def get_realY(index):
+def get_realY(index, batch_size):
     result = []
-    for i in range(classification_len):
-        if i == index:
-            result.append(1)
-        else:
-            result.append(0)
+    for j in range(batch_size):
+        result.append([])
+        for i in range(classification_len):
+            if i == index[j]:
+                result[j].append(1)
+            else:
+                result[j].append(0)
     return result
 
 
-class AlexNet(object):
-    def __init__(self, input_x, keep_prob, num_classes, weights_path='alexnet.npy'):
-        self.input_x = input_x
-        self.keep_prob = keep_prob
-        self.weights_path = weights_path
-        self.num_classes = num_classes
+def conv2d(x, w, stride):
+    return tf.nn.conv2d(x, w, strides=[1, stride, stride, 1], padding="SAME")
 
-    def conv(self, x, kernel_height, num_kernels, stride, padding='SAME', padding_num=0, groups=1):
-        input_channels = int(np.shape(x)[-1])
-        if not padding_num == 0:
-            x = tf.pad(x, [[0, 0], [padding_num, padding_num], [padding_num, padding_num], [0, 0]])
-        weights_initial = tf.truncated_normal(
-            shape=[kernel_height, kernel_height, input_channels, num_kernels], stddev=0.1)
-        weights = tf.Variable(weights_initial)
-        biases_initial = tf.constant(0.1, shape=[num_kernels])
-        biases = tf.Variable(biases_initial)
 
-        convolve = lambda i, k: tf.nn.conv2d(i, k, strides=[1, stride, stride, 1], padding=padding)
-        if groups == 1:
-            conv = convolve(x, weights)
+# def conv(x, kernel_height, num_kernels, stride, padding='SAME'):
+#     input_channels = int(np.shape(x)[-1])
+#     weights_initial = tf.truncated_normal(
+#         shape=[kernel_height, kernel_height, input_channels, num_kernels], stddev=0.1)
+#     weights = tf.Variable(weights_initial)
+#     biases_initial = tf.constant(0.1, shape=[num_kernels])
+#     biases = tf.Variable(biases_initial)
+#
+#     convolve = lambda i, k: tf.nn.conv2d(i, k, strides=[1, stride, stride, 1], padding=padding)
+#     conv = convolve(x, weights)
+#
+#     # add biases and active function
+#     withBias = tf.add(conv, biases)
+#     relu = tf.nn.relu(withBias)
+#     return relu
+
+
+def maxPooling(input, filter_size, stride, padding='SAME'):
+    return tf.nn.max_pool(input, ksize=[1, filter_size, filter_size, 1], strides=[1, stride, stride, 1],
+                          padding=padding)
+
+
+def lrn(input, radius, alpha, beta, bias=1.0):
+    return tf.nn.local_response_normalization(input, depth_radius=radius, alpha=alpha, beta=beta, bias=bias)
+
+
+def fc(input, num_in, num_out, drop_ratio=0, relu=True):
+    weights = weight_variable([num_in, num_out])
+    biases = bias_variable([num_out])
+    # Linear
+    act = tf.matmul(input, weights) + biases
+
+    if relu == True:
+        relu = tf.nn.relu(act)
+        if drop_ratio == 0:
+            return relu
         else:
-            input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
-            weights_groups = tf.split(axis=3, num_or_size_splits=groups, value=weights)
-            output_groups = [convolve(i, k) for i, k in zip(input_groups, weights_groups)]
-
-            conv = tf.concat(axis=3, values=output_groups)
-
-        # add biases and active function
-        withBias = tf.add(conv, biases)
-        relu = tf.nn.relu(withBias)
-        return relu
-
-    def maxPooling(self, input, filter_size, stride, padding='SAME'):
-        return tf.nn.max_pool(input, ksize=[1, filter_size, filter_size, 1], strides=[1, stride, stride, 1],
-                              padding=padding)
-
-    def lrn(self, input, radius, alpha, beta, bias=1.0):
-        return tf.nn.local_response_normalization(input, depth_radius=radius, alpha=alpha, beta=beta, bias=bias)
-
-    def fc(self, input, num_in, num_out, drop_ratio=0, relu=True):
-        weights_initial = tf.truncated_normal(
-            shape=[num_in, num_out], stddev=0.1)
-        weights = tf.Variable(weights_initial)
-        biases_initial = tf.constant(0.1, shape=[num_out])
-        biases = tf.Variable(biases_initial)
-        # Linear
-        act = tf.nn.xw_plus_b(input, weights, biases)
-
-        if relu == True:
-            relu = tf.nn.relu(act)
-            if drop_ratio == 0:
-                return relu
-            else:
-                return tf.nn.dropout(relu, 1.0 - drop_ratio)
+            return tf.nn.dropout(relu, 1.0 - drop_ratio)
+    else:
+        if drop_ratio == 0:
+            return act
         else:
-            if drop_ratio == 0:
-                return act
-            else:
-                return tf.nn.dropout(act, 1.0 - drop_ratio)
+            return tf.nn.dropout(act, 1.0 - drop_ratio)
 
-    def pred(self):
-        # layer 1
-        conv1 = self.conv(self.input_x, 11, 96, 4, padding='VALID')
-        pool1 = self.maxPooling(conv1, filter_size=3, stride=2, padding='VALID')
-        norm1 = self.lrn(pool1, 2, 2e-05, 0.75)
-        # layer 2
-        conv2 = self.conv(norm1, 5, 256, 1, padding_num=0, groups=2)
-        pool2 = self.maxPooling(conv2, filter_size=3, stride=2, padding='VALID')
-        norm2 = self.lrn(pool2, 2, 2e-05, 0.75)
-        # layer 3
-        conv3 = self.conv(norm2, 3, 384, 1)
-        # layer 4
-        conv4 = self.conv(conv3, 3, 384, 1, groups=2)
-        # layer 5
-        conv5 = self.conv(conv4, 3, 256, 1, groups=2)
-        pool5 = self.maxPooling(conv5, filter_size=3, stride=2, padding='VALID')
-        # layer 6
-        flattened = tf.reshape(pool5, [-1, 6 * 6 * 256])
-        fc6 = self.fc(input=flattened, num_in=6 * 6 * 256, num_out=4096, drop_ratio=1.0 - self.keep_prob,
-                      relu=True)
-        # layer 7
-        fc7 = self.fc(input=fc6, num_in=4096, num_out=4096, drop_ratio=1.0 - self.keep_prob, relu=True)
-        # layer 8
-        fc8 = self.fc(input=fc7, num_in=4096, num_out=self.num_classes, drop_ratio=0, relu=False)
-        return fc8
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 
 input_x = tf.placeholder(tf.float32, [None, 227 * 227 * 3])
 input_x = tf.reshape(input_x, [-1, 227, 227, 3])
 real_y = tf.placeholder(tf.float32, [None, 8])
-model = AlexNet(input_x, 0.5, 8)
-pred_y = model.pred()
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred_y, real_y))
+keep_prob = tf.placeholder(tf.float32)
+# layer 1
+w_conv1 = weight_variable([11, 11, 3, 96])
+b_conv1 = bias_variable([96])
+h_conv1 = tf.nn.relu(conv2d(input_x, w_conv1, 4) + b_conv1)
+pool1 = maxPooling(h_conv1, filter_size=3, stride=2, padding='VALID')
+norm1 = lrn(pool1, 2, 2e-05, 0.75)
+# layer 2
+w_conv2 = weight_variable([5, 5, 96, 256])
+b_conv2 = bias_variable([256])
+h_conv2 = tf.nn.relu(conv2d(norm1, w_conv2, 1) + b_conv2)
+pool2 = maxPooling(h_conv2, filter_size=3, stride=2, padding='VALID')
+norm2 = lrn(pool2, 2, 2e-05, 0.75)
+# layer 3
+w_conv3 = weight_variable([3, 3, 256, 384])
+b_conv3 = bias_variable([384])
+h_conv3 = tf.nn.relu(conv2d(norm2, w_conv3, 1) + b_conv3)
+# layer 4
+w_conv4 = weight_variable([3, 3, 384, 384])
+b_conv4 = bias_variable([384])
+h_conv4 = tf.nn.relu(conv2d(h_conv3, w_conv4, 1) + b_conv4)
+# layer 5
+w_conv5 = weight_variable([3, 3, 384, 256])
+b_conv5 = bias_variable([256])
+h_conv5 = tf.nn.relu(conv2d(h_conv4, w_conv5, 1) + b_conv5)
+pool5 = maxPooling(h_conv5, filter_size=3, stride=2, padding='VALID')
+# layer 6
+flattened = tf.reshape(pool5, [-1, 6 * 6 * 256])
+
+fc6 = fc(input=flattened, num_in=6 * 6 * 256, num_out=4096, drop_ratio=1.0 - keep_prob,
+         relu=True)
+# layer 7
+fc7 = fc(input=fc6, num_in=4096, num_out=4096, drop_ratio=1.0 - keep_prob, relu=True)
+# layer 8
+fc8 = fc(input=fc7, num_in=4096, num_out=num_classes, drop_ratio=0, relu=False)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=real_y, logits=fc8))
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+correct_pred = tf.equal(tf.argmax(fc8, 1), tf.argmax(real_y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for step in range(iterator):
         next_batch = get_next_batch()
-        sess.run(optimizer, feed_dict={input_x: next_batch[0], real_y: next_batch[1]})
-        if step % 1 == 0:
-            print(sess.run(accuracy, feed_dict={input_x: next_batch[0], real_y: next_batch[1]}))
+        sess.run(optimizer, feed_dict={input_x: next_batch[0], real_y: next_batch[1], keep_prob: 0.5})
+        if step % 10 == 0:
+            print(sess.run(accuracy, feed_dict={input_x: next_batch[0], real_y: next_batch[1], keep_prob: 0.5}))
